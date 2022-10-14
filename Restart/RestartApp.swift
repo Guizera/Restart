8047695e6d48ce4276b9a71166e1b1793273f78f
Guilherme Cunha <guizera7@gmail.com>
2022-10-14 15:27:08 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by José Alves da Cunha on 14/10/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
